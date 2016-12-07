903bf359f8e1ec825224e6431c25a562880a8dd5
khan133 <khan133@purdue.edu>
2016-12-06 19:00:45 -0500

    Invite other students to group Only students can join groups

--- GroupsApp/forms.py
+++ GroupsApp/forms.py
@@ -7,3 +7,7 @@ from django import forms
 class GroupForm(forms.Form):
     name = forms.CharField(label='Name', max_length=30)
     description = forms.CharField(label='Description', max_length=300)
+
+class InvitationForm(forms.Form):
+    from AuthenticationApp import models
+    student = forms.ModelChoiceField(queryset=models.Student.objects.all(),required=True)
