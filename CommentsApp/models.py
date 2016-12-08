84fa9fa4f25d499a1029abfe57c5c69843772d01
Muhammad Hamza Farrukh <mfarrukh@purdue.edu>
2016-12-08 03:30:38 -0500

    Added Comments in Project

--- CommentsApp/models.py
+++ CommentsApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from AuthenticationApp.models import MyUser
 from GroupsApp.models import Group
 
 
@@ -8,3 +9,4 @@ class Comment(models.Model):
     time = models.DateTimeField(auto_now=True)
     comment = models.CharField(max_length=500)
     group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True)
+    comment_by = models.ForeignKey(MyUser, null=True)
